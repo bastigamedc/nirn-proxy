f1b79a495fec5a54bc8b72368d41656d76e827b1
basti <basti02@yahoo.com>
2025-12-07 13:43:40 +0100

    fix: simplify clientId initialization in QueueManager

--- lib/queue_manager.go
+++ lib/queue_manager.go
@@ -265,10 +265,7 @@ func (m *QueueManager) DiscordRequestHandler(resp http.ResponseWriter, req *http
 	metricsPath := GetMetricsPath(req.URL.Path)
 
 	token := req.Header.Get("Authorization")
-    clientId := ""
-    if clientId == "" {
-    	clientId = GetBotId(token)
-    }
+    clientId := GetBotId(token)
 
 	ConnectionsOpen.With(map[string]string{"route": metricsPath, "method": req.Method, "clientId": clientId}).Inc()
 	defer ConnectionsOpen.With(map[string]string{"route": metricsPath, "method": req.Method, "clientId": clientId}).Dec()
